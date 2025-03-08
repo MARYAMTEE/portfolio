5d893269c76062ffd2286d56c587d254e482a5c8
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-03-08 16:48:33 +0100

    update navigate bar

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+
+const menuOpenButton = document.querySelector("#open-menu-btn");
+const menuCloseButton = document.querySelector("#close-menu-btn");
+
+
+// Toggle Visibility
+menuOpenButton.addEventListener("click", () => {
+	document.body.classList.toggle("show-mobile-menu");
+});
+
+// Closes menu button
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
